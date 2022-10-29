d01e0d166287afe372a22221039013a24868fa85
stark-2000 <dhineshrajasekaran@gmail.com>
2022-10-29 15:57:56 -0400

    Human Tracker function update - added unique ID & label for detected human

--- app/openCV.cpp
+++ app/openCV.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+
 using std::cout;
 using std::endl;
 using std::vector;
+using std::string;
+using std::to_string;
 
 using namespace cv;
 using cv::Scalar;
@@ -28,11 +31,13 @@ int main (int argc, const char * argv[])
   {
     cout << "Video Source not avalable !" << endl;
   }
-    
+  
+  // Histogram of Gradients Method for Human Detection
   Mat img;
   HOGDescriptor hog;
   hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
 
+  // Capturing a frame from video
   namedWindow("video capture", WINDOW_AUTOSIZE);
   while (true) {
     video >> img;
@@ -42,7 +47,8 @@ int main (int argc, const char * argv[])
     vector<Rect> found, found_filtered;
     hog.detectMultiScale(img, found, 0, Size(8, 8), Size(32, 32), 1.05, 2);
 
-    size_t i, j;
+    // Multiple human detection & storing data in vector
+    int i, j;
     for (i = 0; i < found.size(); i++) {
       Rect r = found[i];
       for (j = 0; j < found.size(); j++)
@@ -51,6 +57,8 @@ int main (int argc, const char * argv[])
       if (j == found.size())
         found_filtered.push_back(r);
     }
+
+    // Drawing bouding box for each human detected
     for (i = 0; i < found_filtered.size(); i++) {
       Rect r = found_filtered[i];
       r.x += cvRound(r.width * 0.1);
@@ -58,21 +66,34 @@ int main (int argc, const char * argv[])
       r.y += cvRound(r.height * 0.06);
       r.height = cvRound(r.height * 0.9);
       rectangle(img, r.tl(), r.br(), Scalar(0, 255, 0), 2);
+
+      string ftext = "Human ";
+      string dtext = to_string(i+1);
+      string text = ftext + dtext;
+
+      putText(img, text, Point(r.tl().x,r.tl().y-10), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 2);
+
       a[i] = (r.tl().x + r.br().x)/2;
       b[i] = (r.tl().y + r.br().y)/2;
     }
 
-    cout << "Human 1: " << a[0] << "," << b[0] << endl;
-    cout << "Human 2: " << a[1] << "," << b[1] << endl;
+    // Displaying no of humans detected 
+    cout << "No. of Humans: " << i << endl;
 
-    a[0] = 0;
-    a[1] = 0;
-    b[0] = 0;
-    b[1] = 0;
+    // Displaying pixel coordinates of each human detected
+    for (i=0; i < found_filtered.size(); i++)
+    {
+       cout << "Human " << i+1 << ": ";
+       cout << a[i] << "," << b[i] << endl;
+    }
 
+    cout << endl;
+
+    // Overlay info on real-time video
     imshow("video capture", img);
     if (waitKey(20) >= 0)
       break;
     }
+    
     return 0;
 }
